b6d1b94c4d7278dc2d08103541db0ba4958cb07d
Dingadung <1229jimin@naver.com>
2023-02-20 16:24:37 +0900

    Delete: genre column in Book model

--- backend/book_jeok/books/models.py
+++ backend/book_jeok/books/models.py
@@ -6,12 +6,12 @@ from django.db import models
 class Book(models.Model):
     """책 모델"""
     book_id = models.BigAutoField(primary_key=True)  # pk로 설정
-    title = models.CharField(max_length=255)
-    publisher = models.CharField(max_length=255)
-    genre = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
-    pubdate = models.DateTimeField()
+    title = models.CharField(max_length=255) # API 책이름
+    publisher = models.CharField(max_length=255) # API 출판사
+    # genre = models.CharField(max_length=255) # API로 못가져옴
+    author = models.CharField(max_length=255) # API 작가이름
+    description = models.CharField(max_length=255) # API 간단 설명
+    pubdate = models.DateTimeField() # API 출간일
 
 
 class Attachment(models.Model):
