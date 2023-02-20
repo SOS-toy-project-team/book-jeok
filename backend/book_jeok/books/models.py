2ea0c085a4fd589285232119bb2c844d75b2b856
Dingadung <1229jimin@naver.com>
2023-02-20 17:15:05 +0900

    Add: Attachement model에 book_id 외래키 추가 in books App

--- backend/book_jeok/books/models.py
+++ backend/book_jeok/books/models.py
@@ -18,5 +18,6 @@ class Attachment(models.Model):
     """책 첨부파일 모델"""
     file_no = models.BigAutoField(primary_key=True)  # pk로 설정
     path = models.CharField(max_length=255)
+    book_id = models.ForeignKey('books.Book')
     """FileField라는 파일관련 모델 필드가 있어서 혹시 몰라 주석 처리 해서 넣어놨습니다."""
     # path = models.FileField()
